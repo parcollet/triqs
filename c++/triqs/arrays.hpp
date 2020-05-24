841b025484210c759ff5b9bff8d0a76dd66d12c1
Olivier Parcollet <olivier.parcollet@cea.fr>
2020-05-24 19:46:37 -0400

    WIP

--- c++/triqs/arrays.hpp
+++ c++/triqs/arrays.hpp
@@ -30,19 +30,36 @@
 //#include <nda/blas.hpp>
 #include <nda/lapack.hpp>
 
-namespace triqs::arrays { 
+#define TRIQS_CLEF_MAKE_FNT_LAZY CLEF_MAKE_FNT_LAZY
+#define TRIQS_CLEF_IMPLEMENT_LAZY_CALL CLEF_IMPLEMENT_LAZY_CALL
+
+// FIXME
+#ifndef TRIQS_RUNTIME_ERROR
+#define TRIQS_RUNTIME_ERROR NDA_RUNTIME_ERROR
+#endif
+
+namespace triqs::utility {
+
+  template <typename T, int R> using mini_vector = std::array<T, R>;
+
+}
+
+namespace triqs::arrays {
 
   using namespace nda;
 
+  using utility::mini_vector;
 
- template<typename T> 
-   using vector = nda::array<T, 1>;
+  template <typename T> using vector = nda::array<T, 1>;
 
- template<typename T> 
-   using vector_view = nda::array_view<T, 1>;
+  template <typename T> using vector_view = nda::array_view<T, 1>;
 
+  template <typename T>
 
+  nda::matrix<T> make_unit_matrix(int dim) {
+    return nda::eye<T>(dim);
+  }
 
+  template <typename... T> mini_vector<size_t, sizeof...(T)> make_shape(T... args) { return {args...}; }
 
-
-}
+} // namespace triqs::arrays
